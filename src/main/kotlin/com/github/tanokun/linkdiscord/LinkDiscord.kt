c9040cd0bcdab45cebfc28550d3e025b8b5020ed
tanoKun <76431283+tanokun@users.noreply.github.com>
2022-11-24 20:31:18 +0900

    Init

---
+++ src/main/kotlin/com/github/tanokun/linkdiscord/LinkDiscord.kt
@@ -0,0 +1,12 @@
+package com.github.tanokun.linkdiscord
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LinkDiscord : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+    override fun onDisable() {
+
+    }
+}
